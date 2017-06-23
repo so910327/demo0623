22240fdc4d9cde987f535ebf53adcbfbdd20f4c3
TingRu.Chen <so910327@gmail.com>
2017-06-23 17:10:21 +0800

    test commit

--- test/api/controllers/addone.js
+++ test/api/controllers/addone.js
@@ -1,48 +1,47 @@
-/*var should = require('should');
+var should = require('should');
 var request = require('supertest');
 var server = require('../../../app');
 
 describe('controllers', function() {
 
-  describe('hello_world', function() {
+  describe('addone', function() {
 
-    describe('GET /hello', function() {
+    describe('GET /addone', function() {
 
-      it('should return a default string', function(done) {
+      it('should return a default number', function(done) {
 
         request(server)
-          .get('/hello')
+          .get('/addone')
           .set('Accept', 'application/json')
           .expect('Content-Type', /json/)
           .expect(200)
           .end(function(err, res) {
             should.not.exist(err);
-
-            res.body.should.eql('Hello, stranger!');
+            //console.log("excuting");
+            res.body.should.eql('0 after addone, now total: 1');
 
             done();
           });
       });
 
-      it('should accept a name parameter', function(done) {
+      it('should input number', function(done) {
 
         request(server)
-          .get('/hello')
-          .query({ name: 'Scott'})
+          .get('/addone')
           .set('Accept', 'application/json')
           .expect('Content-Type', /json/)
           .expect(200)
-          .end(function(err, res) {
+          .end(function(err, res, req) {
             should.not.exist(err);
-
-            res.body.should.eql('Hello, Scott!');
+            res.body.should.eql('0 after addone, now total: 1');
 
             done();
           });
       });
+
 
     });
 
   });
 
-});*/
+});
